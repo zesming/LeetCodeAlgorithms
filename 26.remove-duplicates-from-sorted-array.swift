5ba382cc0c42ea00c23c77cf68098e40f48577be
zesming <ming9010@gmail.com>
2019-03-06 23:47:05 +0800

    [2019.03.06]

---
+++ 26.remove-duplicates-from-sorted-array.swift
@@ -0,0 +1,103 @@
+/*
+ * @lc app=leetcode id=26 lang=swift
+ *
+ * [26] Remove Duplicates from Sorted Array
+ *
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+ *
+ * algorithms
+ * Easy (39.55%)
+ * Total Accepted:    530.7K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '[1,1,2]'
+ *
+ * Given a sorted array nums, remove the duplicates in-place such that each
+ * element appear only once and return the new length.
+ * 
+ * Do not allocate extra space for another array, you must do this by modifying
+ * the input array in-place with O(1) extra memory.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Given nums = [1,1,2],
+ * 
+ * Your function should return length = 2, with the first two elements of nums
+ * being 1 and 2 respectively.
+ * 
+ * It doesn't matter what you leave beyond the returned length.
+ * 
+ * Example 2:
+ * 
+ * 
+ * Given nums = [0,0,1,1,1,2,2,3,3,4],
+ * 
+ * Your function should return length = 5, with the first five elements of nums
+ * being modified to 0, 1, 2, 3, and 4 respectively.
+ * 
+ * It doesn't matter what values are set beyond the returned length.
+ * 
+ * 
+ * Clarification:
+ * 
+ * Confused why the returned value is an integer but your answer is an array?
+ * 
+ * Note that the input array is passed in by reference, which means
+ * modification to the input array will be known to the caller as well.
+ * 
+ * Internally you can think of this:
+ * 
+ * 
+ * // nums is passed in by reference. (i.e., without making a copy)
+ * int len = removeDuplicates(nums);
+ * 
+ * // any modification to nums in your function would be known by the caller.
+ * // using the length returned by your function, it prints the first len
+ * elements.
+ * for (int i = 0; i < len; i++) {
+ * print(nums[i]);
+ * }
+ * 
+ */
+class Solution {
+    // Type 1, remove duplicates
+    // func removeDuplicates(_ nums: inout [Int]) -> Int {
+    //     var removedCount = 0
+    //     var flagElement = 0
+
+    //     for (index, element) in nums.enumerated() {
+    //         if index == 0 {
+    //             flagElement = element
+    //         } else {
+    //             if element == flagElement {
+    //                 nums.remove(at: index - removedCount)
+    //                 removedCount += 1
+    //             } else {
+    //                 flagElement = element
+    //             }
+    //         }
+    //     }
+
+    //     return nums.count
+    // }
+
+    // Type 2, change duplicates index
+    func removeDuplicates(_ nums: inout [Int]) -> Int {
+        guard nums.count > 0 else {
+            return 0
+        }
+
+        var preIndex = 0
+        var postIndex = 1
+        for _ in 1..<nums.count {
+            if nums[postIndex] != nums[preIndex] {
+                preIndex += 1
+                nums[preIndex] = nums[postIndex]
+            }
+            postIndex += 1
+        }
+        
+        return preIndex + 1
+    }
+}
+
